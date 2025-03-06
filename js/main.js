c5931be540e44f36565825472b8bb8725a491966
wan <conut22@naver.com>
2025-03-06 23:51:19 +0900

    ✨ feat     : sort cards by creation and latest order

--- js/main.js
+++ js/main.js
@@ -26,4 +26,66 @@ function renderColumnsAndCards(columnData) {
   });
 }
 
+function clearColumnsAndCards() {
+  const parentElement = document.querySelector('#columns-container');
+  parentElement.innerHTML = ''; // 기존 요소들 모두 삭제
+}
+
+//정렬 버튼 클릭 이벤트
+function sortCards(event) {
+  const sortButton = event.currentTarget;
+  const sortButtonName = sortButton.querySelector('.sort-btn-name');
+  const currentType = sortButton.dataset.type;
+
+  // 상태 변경 및 버튼 텍스트 업데이트 함수
+  const updateButtonState = (newType, buttonText) => {
+    sortButton.dataset.type = newType;
+    sortButtonName.textContent = buttonText;
+  };
+
+  // 정렬 기준에 맞는 데이터 반환 함수
+  const getSortedData = (type) => {
+    return type === 'created'
+      ? sortTasksByCreationOrder(mockData.columns)
+      : sortTasksByLatest(mockData.columns);
+  };
+
+  // 데이터 정렬
+  const sortedData = getSortedData(currentType);
+
+  // 상태 업데이트 및 랜더링
+  if (currentType === 'created') {
+    updateButtonState('latest', '최신순');
+  } else {
+    updateButtonState('created', '생성순');
+  }
+
+  // 기존 데이터 제거 후 정렬된 데이터로 랜더링
+  clearColumnsAndCards();
+  renderColumnsAndCards(sortedData);
+}
+
+//생성순 정렬
+function sortTasksByCreationOrder(columnData) {
+  return columnData.map((column) => ({
+    ...column,
+    tasks: [...column.tasks].sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    ),
+  }));
+}
+
+//최신순 정렬
+function sortTasksByLatest(columnData) {
+  return columnData.map((column) => ({
+    ...column,
+    tasks: [...column.tasks].sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    ),
+  }));
+}
+
 renderColumnsAndCards(mockData.columns);
+
+const sortButton = document.getElementById('sort-btn');
+sortButton.addEventListener('click', sortCards);
