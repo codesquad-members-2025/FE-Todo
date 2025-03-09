5a61db4ff2a7679949bfa4567560287ac4ccde1b
jicho <jIcho6412@gmail.com>
2025-03-09 15:31:16 +0900

    refactor(historyList): 함수를 만들어 처리

---
+++ eventHandler/historyList.js
@@ -0,0 +1,17 @@
+const openButton = document.getElementById("historyOpenButton");
+const historyListLayer = document.getElementById("historyList");
+const closedButton = document.getElementById("history__closedButton");
+
+// 히스토리 리스트 열기
+function openHistoryList() {
+  historyListLayer.classList.add("active");
+}
+
+// 히스토리 리스트 닫기
+function closeHistoryList() {
+  historyListLayer.classList.remove("active");
+}
+
+// 이벤트 설정
+openButton.addEventListener("click", openHistoryList);
+closedButton.addEventListener("click", closeHistoryList);
