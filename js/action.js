eb24483533d6227041bd78b3272a22c2f777c1a5
jicho <jIcho6412@gmail.com>
2025-03-09 15:15:30 +0900

    feat(historyList): 닫기 버튼 클릭시 해당 레이어 제거

--- js/action.js
+++ js/action.js
@@ -4,3 +4,9 @@ const historyListLayer = document.getElementById("historyList");
 openButton.addEventListener("click", function () {
   historyListLayer.classList.add("active"); // active 클래스를 추가하여 슬라이드 애니메이션 적용
 });
+
+const closedButton = document.getElementById("history__closedButton");
+closedButton.addEventListener("click", function () {
+  historyListLayer.classList.remove("active");
+  // active 클래스를 제거
+});
