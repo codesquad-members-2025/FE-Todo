17238fe2c41f5fecfcb710097bae20c5f0267b4c
wan <conut22@naver.com>
2025-03-10 23:05:00 +0900

    ✨ feat: load column data using fetch with mock JSON
    - Replaced hardcoded column data with mock JSON
    - Implemented fetch to dynamically load column data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import columnData from '../data/columnData.js';
+// import columnData from '../data/columnData.js';
 import historyData from '../data/historyData.js';
 import {
   renderColumnsAndCards,
@@ -7,17 +7,32 @@ import {
 import { initHistoryButton, renderHistoryItems } from './components/history.js';
 import { initSortButton } from './components/sort.js';
 
-//Sort Button
-initSortButton(columnData);
+//Fetch Mockdata
+async function fetchColumnData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('네트워크 응답에 문제가 있습니다.');
+    }
+    const data = await response.json();
+    processColumnData(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+function processColumnData(data) {
+  initSortButton(data);
+  renderColumnsAndCards(data);
+}
+
+fetchColumnData('./data/columnData.json');
 
 //History Button
 initHistoryButton();
 
 //Card Remove Button
 initCardRemoveButton();
-
-//First render
-renderColumnsAndCards(columnData);
 
 //history Items render
 renderHistoryItems(historyData);
