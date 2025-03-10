5a93bb43e0d858bcebcdb92e059f3d8d8445f2da
youngmin <dudals2963@tukrorea.ac.kr>
2025-03-10 17:44:37 +0900

    히스토리 팝업바 열기& 닫기& 활동내역 삭제 버튼 클릭시 동작구현 완료. 하지만 DOM트리를 효율적으로 탐색하면서 기능을 구현해야한다.

---
+++ js_dir/control-Historybar.js
@@ -0,0 +1,38 @@
+const historyBtn = document.querySelector(".header__history-btn");
+const historySidebar = document.querySelector(".popover-sidebar");
+const closeBtn = historySidebar.querySelector(".popover-header__closeBtn");
+const deleteBtn = historySidebar.querySelector(".delete-sidebar_button");
+const deleteModal = document.querySelector("#delete-history__modal");
+
+historyBtn.addEventListener("click", () => {
+  historySidebar.classList.toggle("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  historySidebar.classList.remove("open");
+});
+deleteBtn.addEventListener("click", () => {
+  deleteModal.showModal();
+
+  deleteModal.querySelector("#cancel-button").addEventListener("click", () => {
+    deleteModal.close();
+  });
+
+  deleteModal
+    .querySelector("#confirm-delete-button")
+    .addEventListener("click", () => {
+      const logSection = historySidebar.querySelector(".activity-list__ul");
+
+      if (logSection) {
+        logSection.innerHTML = "";
+      }
+      const emptyMessage = historySidebar.querySelector(
+        ".popover__empty-message"
+      );
+      if (emptyMessage) {
+        emptyMessage.style.display = "block";
+      }
+
+      deleteModal.close();
+    });
+});
