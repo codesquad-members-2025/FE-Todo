172c50ad4d941e6fdb802a1e51473e7c74e9b972
jicho <jIcho6412@gmail.com>
2025-03-10 22:20:18 +0900

    feat: 삭제 알림창에서 취소 버튼 클릭 시 기록 목록으로 되돌아가기

--- eventHandler/historyList.js
+++ eventHandler/historyList.js
@@ -2,6 +2,7 @@ const openButton = document.getElementById("historyOpenButton");
 const historyListLayer = document.getElementById("historyList-noEmpty");
 const closedButton = document.getElementById("history__closedButton");
 const deleteButton = document.getElementById("delete");
+const cancleButton = document.getElementById("cancleButton");
 
 // 히스토리 리스트 열기
 function openHistoryList() {
@@ -20,7 +21,17 @@ function confirmDeleteAlert() {
   overlay.style.display = "block";
   alert.style.display = "block";
 }
+
+// 삭제 알림창에서 취소 버튼 클릭 시 알림창 제거
+function cancleDelete() {
+  const overlay = document.getElementById("overlay-dark");
+  const alert = document.getElementById("alert-box");
+  overlay.style.display = "none";
+  alert.style.display = "none";
+}
+
 // 이벤트 설정
 openButton.addEventListener("click", openHistoryList);
 closedButton.addEventListener("click", closeHistoryList);
 deleteButton.addEventListener("click", confirmDeleteAlert);
+cancleButton.addEventListener("click", cancleDelete);
