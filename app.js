e2f6e3de5275ed0cdd0e4d291022feaeef6e3fa0
이상윤 <sangyoonlee1231@isang-yun-ui-MacBookPro.local>
2025-03-11 16:09:39 +0900

    Modify: (드디어) 모달 창 버그 수정

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
 // 히스토리 모달 등장하거나 사라지는 구현
 const historyOpenBtn = document.querySelector(".header__history-icon");
 const historyModal = document.querySelector(".history-modal-container");
-const historyModalCloseBtn = document.querySelector(
-  ".history-modal__header-close"
-);
+const historyCloseBtn = document.querySelector(".history-modal__header-close");
 
-historyOpenBtn.addEventListener("click", () => {
-  historyModal.style.display = "grid";
-  historyModal.style.animation = "slideIn 0.5s forwards";
-});
-historyModalCloseBtn.addEventListener("click", () => {
-  historyModal.style.animation = "slideOut 0.3s forwards";
+// 히스토리 모달 열고 닫기
+const openHistoryModal = () => {
+  historyModal.style.cssText = `
+    display: grid;
+    animation: slideIn 0.5s forwards;
+  `;
+};
+
+const closeHistoryModal = () => {
+  historyModal.style.cssText = "animation: slideOut 0.3s forwards";
   setTimeout(() => {
     historyModal.style.display = "none";
   }, 500);
-});
+};
+
+historyOpenBtn.addEventListener("click", openHistoryModal);
+historyCloseBtn.addEventListener("click", closeHistoryModal);
 
 //
 
 // 히스토리 삭제 모달 이벤트
 const historyDeleteBtn = document.querySelector(".history-modal__footer-btn");
 const historyDeleteModal = document.querySelector(".history-delete-modal");
-const historyDeleteModalCancelBtn =
-  document.querySelector("#cancel-delete-btn");
-const historyDeleteModalDeleteBtn = document.querySelector(
-  "#confirm-delete-btn"
-);
+const historyDeleteCancelBtn = document.querySelector("#cancel-delete-btn");
+const historyDeleteApproveBtn = document.querySelector("#confirm-delete-btn");
 
-// 모달 열기
-historyDeleteBtn.addEventListener("click", () => {
+historyDeleteModal.close();
+
+// 모달 열고 닫기
+const showModal = () => {
   historyDeleteModal.showModal();
-});
+};
 
-// 모달 닫기
-historyDeleteModalCancelBtn.addEventListener("click", () => {
+const deleteModal = () => {
   historyDeleteModal.close();
-});
+};
+
+historyDeleteBtn.addEventListener("click", showModal);
+historyDeleteCancelBtn.addEventListener("click", deleteModal);
 
 // 모든 사용자 기록 삭제
-historyDeleteModalDeleteBtn.addEventListener("click", () => {
+historyDeleteApproveBtn.addEventListener("click", () => {
   // 사용자 기록 삭제
   // 사용자 기록 삭제 후 모달 닫기
   historyDeleteModal.close();
