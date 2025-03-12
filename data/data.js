fed1bac7f65e63d7a2944968164757ba686a4659
jicho <jIcho6412@gmail.com>
2025-03-12 21:15:29 +0900

    feat(toDo-cardList): mock데이터를 만들어서 노출하게끔 구현

---
+++ data/data.js
@@ -0,0 +1,25 @@
+export const data = {
+  colums: [
+    {
+      id: "toDo",
+      count: 0,
+      cardList: [
+        {
+          title: "GitHub 공부하기",
+          content: "add, commit, push",
+          author: "author by web",
+        },
+        {
+          title: "블로그에 포스팅할 것",
+          content: "* GitHub 공부내용 \n * 모던 자바스크립트 1장 공부내용",
+          author: "author by web",
+        },
+        {
+          title: "GitHub 공부하기",
+          content: "add, commit, push",
+          author: "author by web",
+        },
+      ],
+    },
+  ],
+};
