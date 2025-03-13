684e7968d9564e39df505e5ba9a0567ab3b1049b
youngmin <dudals2963@tukrorea.ac.kr>
2025-03-13 19:55:16 +0900

    사용자 입력값 localStorage에서 받아오고, 기존 목업json파일에서 fetch로 받아온 데이터 병합 구현, 새로고침시 기존 투두 카드 화면에 업로드 구현

--- js_dir/mergerObj.js
+++ js_dir/mergerObj.js
@@ -1,4 +1,8 @@
+//로컬 스토리지 & 목업 데이터 두개 머기 함수... 사실 스토어 개념 있으면 로컬 스토리지 필요 없음
+
 export function mergeObjects(obj1, obj2) {
+  obj1 = obj1 || {};
+  obj2 = obj2 || {};
   const merged = { ...obj1 };
   Object.keys(obj2).forEach((key) => {
     if (Object.hasOwn(merged, key)) {
