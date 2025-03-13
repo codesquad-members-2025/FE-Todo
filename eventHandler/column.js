5213fa64fbd9e326226595880631980a11f6c93d
jicho <jIcho6412@gmail.com>
2025-03-13 20:56:05 +0900

    refactor: 카드 추가 폼을 만드는 기능 분리

--- eventHandler/column.js
+++ eventHandler/column.js
@@ -10,34 +10,39 @@ export function eventAddCard() {
       const columnList = event.target.closest(".column-header");
       if (columnList && columnList.nextElementSibling) {
         const fragment = document.createDocumentFragment();
-        const addCardForm = document.createElement("div");
-        addCardForm.classList.add("add-card");
-        addCardForm.innerHTML = `<div class="add-card__input">
-  <input
-    type="text"
-    class="add-card__input__title"
-    placeholder="제목을 입력하세요"
-  />
-  <textarea
-    class="add-card__input__content"
-    rows="1"
-    maxlength="500"
-    oninput="resizeHeight(this)"
-    placeholder="내용을 입력하세요"
-    wrap="hard"
-  ></textarea>
-</div>
-<div class="add-card__buttons">
-  <button class="add-card__cancle-btn">취소</button>
-  <button class="add-card__submit-btn">등록</button>
-</div>
-</div>`;
+        const addCardForm = createAddCardForm();
         fragment.appendChild(addCardForm);
         columnList.nextElementSibling.prepend(fragment);
         processAddCard(addCardForm);
       }
     }
   });
+}
+
+function createAddCardForm() {
+  const addCardForm = document.createElement("div");
+  addCardForm.classList.add("add-card");
+  addCardForm.innerHTML = `<div class="add-card__input">
+<input
+type="text"
+class="add-card__input__title"
+placeholder="제목을 입력하세요"
+/>
+<textarea
+class="add-card__input__content"
+rows="1"
+maxlength="500"
+oninput="resizeHeight(this)"
+placeholder="내용을 입력하세요"
+wrap="hard"
+></textarea>
+</div>
+<div class="add-card__buttons">
+<button class="add-card__cancle-btn">취소</button>
+<button class="add-card__submit-btn">등록</button>
+</div>
+</div>`;
+  return addCardForm;
 }
 
 function processAddCard(addCardForm) {
