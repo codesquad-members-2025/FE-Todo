66815e6139b071eb8b5c6285c883daa7816e800e
이상윤 <sangyoonlee1231@isang-yun-ui-MacBookPro.local>
2025-03-17 11:14:28 +0900

    Modify: 크롱 피드백 반영하여 수정 - 이벤트 위임 스위치 문을 객체 리터럴 방식으로, event.target 지역 변수화

--- src/data.js
+++ src/data.js
@@ -1,17 +1,7 @@
-import { columnsComponent } from "./components/columns_component";
-
 // 칼럼 동적으로 생성하기
 // 비동기로 mock.json 불러오기
-const fetchMockData = async () => {
+export const fetchMockData = async () => {
   const response = await fetch("/data/mock.json");
   const data = await response.json();
   return data;
-};
-
-export const renderColumns = async () => {
-  const data = await fetchMockData();
-
-  // 불러온 데이터를 컴포넌트에 넣어서 렌더링
-  const columnContainer = document.querySelector(".column-container");
-  columnContainer.innerHTML += columnsComponent(data);
 };
